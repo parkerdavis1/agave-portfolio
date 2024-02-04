3d121dc7d5607fee099531757140e7b72237b9c7
Parker <parkied@gmail.com>
2024-02-04 12:42:35 -0700

    Fix copy button positioning

--- public/scripts/copyButton.js
+++ public/scripts/copyButton.js
@@ -3,11 +3,20 @@ const copyButtonLabel = "Copy";
 let blocks = document.querySelectorAll("pre");
 
 blocks.forEach((block) => {
+	console.log("doung something");
 	if (navigator.clipboard) {
+		let parentNode = block.parentNode;
 		let button = document.createElement("button");
+		// let container = document.createElement("div");
+		// container.classList.add("code-container");
 
 		button.innerText = copyButtonLabel;
-		block.appendChild(button);
+		parentNode.appendChild(button);
+		// parentNode.appendChild(container);
+		// container.appendChild(button);
+
+		// block.appendChild(container);
+		// container.appendChild(button);
 
 		button.addEventListener("click", async () => {
 			await copyCode(block);
